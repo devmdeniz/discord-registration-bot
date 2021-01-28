323971eaede0f81badbed6e5c526ac9f38bf6f4a
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-28 09:55:03 +0300

    ready.js translated to english

--- events/ready.js
+++ events/ready.js
@@ -1,20 +1,20 @@
 const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
-const ayarlar = require('../settings.json');
-var surum = ayarlar.surum
-var prefix = ayarlar.prefix;
+const settings = require('../settings.json');
+var version = settings.version
+var prefix = settings.prefix;
 
 module.exports = client => {
-client.user.setActivity(""+prefix+"yardım", {
+client.user.setActivity(""+prefix+"help", {
   type: "STREAMING",
   url: "https://www.twitch.tv/eastromtv"
 });
   
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Commands Loaded!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} Logged In!`);
   client.user.setStatus("LISTENING");
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Game Activity Setted!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Now we have ` + client.channels.size + ` Channels, ` + client.guilds.size + ` Guilds and ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` Users!`);
 
 };
